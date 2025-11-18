1bbe2edd23d3a3f1f17e122a7b3bfd06db290087
MrcsFranca <francamarcos035@gmail.com>
2025-11-18 13:07:16 -0300

    Finishing loadDataBase function

--- AVHash.cpp
+++ AVHash.cpp
@@ -1,17 +1,42 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 #include "libAVL.h"
 
 using namespace std;
 
+node *loadDataBase(node *T, string fileToLoad);
+
 int main() {
     node *T = NULL;
-    T = insertAVL(T, "teste1");
-    T = insertAVL(T, "teste2");
-    T = insertAVL(T, "teste3");
-    T = insertAVL(T, "teste4");
-    inOrderAVL(T);
+
+    T = loadDataBase(T, "signatures.txt");
+
     bool hash;
-    hash = search(T, "teste6");
+    hash = search(T, "fdf483f9a4489655ac803d2ff23294da3eef6bab35340298dac2d7f3889c7724");
     cout << hash << endl;
+}
+
+node *loadDataBase(node *T, string fileToLoad) {
+    string filename(fileToLoad);
+    vector<string> lines;
+    string line;
+
+    ifstream input_file(filename);
+    if(!input_file.is_open()) {
+        cerr << "Não foi possível abrir o arquivo: " << filename << endl;
+        return NULL;
+    }
+
+    while(getline(input_file, line)) {
+        //lines.push_back(line);
+        T = insertAVL(T, line);
+    }
+
+    //for(const auto &i : lines) cout << i << endl;
+
+    input_file.close();
+
+    return T;
 }
