98d03f6ddeea1d7487a81e839c91925a5717794b
MrcsFranca <francamarcos035@gmail.com>
2025-11-19 09:37:46 -0300

    Calculating file hash with pichosha2.h

--- AVHash.cpp
+++ AVHash.cpp
@@ -3,19 +3,25 @@
 #include <fstream>
 #include <vector>
 #include "libAVL.h"
+#include "picosha2.h"
 
 using namespace std;
 
 node *loadDataBase(node *T, string fileToLoad);
+string calculateFileHash(string filePath);
 
 int main() {
     node *T = NULL;
+    bool exists;
+    string hashToSearch;
 
     T = loadDataBase(T, "signatures.txt");
 
-    bool hash;
-    hash = search(T, "fdf483f9a4489655ac803d2ff23294da3eef6bab35340298dac2d7f3889c7724");
-    cout << hash << endl;
+    hashToSearch = calculateFileHash("hashTeste.txt");
+    cout << "hash do arquivo: " << hashToSearch << endl; 
+
+    exists = search(T, hashToSearch);
+    cout << exists << endl;
 }
 
 node *loadDataBase(node *T, string fileToLoad) {
@@ -23,20 +29,43 @@ node *loadDataBase(node *T, string fileToLoad) {
     vector<string> lines;
     string line;
 
-    ifstream input_file(filename);
-    if(!input_file.is_open()) {
+    ifstream inputFile(filename);
+    if(!inputFile.is_open()) {
         cerr << "Não foi possível abrir o arquivo: " << filename << endl;
         return NULL;
     }
 
-    while(getline(input_file, line)) {
+    while(getline(inputFile, line)) {
         //lines.push_back(line);
         T = insertAVL(T, line);
     }
 
     //for(const auto &i : lines) cout << i << endl;
 
-    input_file.close();
+    inputFile.close();
 
     return T;
+}
+
+string calculateFileHash(string filePath) {
+    ifstream file(filePath, ios::binary | ios::ate);
+
+    if(!file.is_open()) {
+        cerr << "Não foi possível abrir o arquivo: " << filePath << endl;
+    }
+
+    ifstream::pos_type pos = file.tellg();
+
+    if(pos == 0) {
+        return picosha2::hash256_hex_string(string(""));
+    }
+
+    vector<char> buffer(pos);
+    file.seekg(0, ios::beg);
+    file.read(&buffer[0], pos);
+    file.close();
+
+    string fileHash;
+    picosha2::hash256_hex_string(buffer, fileHash);
+    return fileHash;
 }
